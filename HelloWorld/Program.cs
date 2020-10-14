af591c3be4281b476fc6a02ae8617d742abebab1
Sasha2018RL <sashakharlamov2004@gmail.com>
2020-10-14 22:59:54 +0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
